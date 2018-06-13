6b6dfe237547c39d82576339edbdf9a72dd2d6ec
Russell Davies <russell@zeroflux.net>
2018-06-13 22:05:22 +0100

    Simplify webpack config

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-'use strict';
+import { pem2jwk } from "pem-jwk";
+import { js_beautify } from "js-beautify";
+import "purecss";
+import "./styles.css";
 
-require("./styles.scss");
-
-var pem2jwk = require('pem-jwk').pem2jwk
-var beautify = require('js-beautify').js_beautify
-
-var Elm = require('./Main');
-var app = Elm.Main.fullscreen();
-
+const Elm = require("Main.elm");
+const app = Elm.Main.fullscreen();
 
 app.ports.convertToJwk.subscribe(function(args) {
-  let [ key, extras ] = args
+  let [key, extras] = args;
   try {
-    let jwk = pem2jwk(key, extras)
-    app.ports.receiveJwk.send(beautify(JSON.stringify(jwk), { indent_size: 2 }))
+    let jwk = pem2jwk(key, extras);
+    console.log(jwk);
+    app.ports.receiveJwk.send(
+      js_beautify(JSON.stringify(jwk), { indent_size: 2 })
+    );
   } catch (e) {
-    app.ports.receiveJwk.send('Invalid input')
+    app.ports.receiveJwk.send("Invalid input");
   }
 });
